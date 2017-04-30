456fa3d7da36216bb6e8fb22763ddeda9e13ae5c
Valeron <valerii.kuzivanov@gmail.com>
2017-04-30 13:32:15 +0600

    styles fix

--- wp-content/themes/plaza/header-kg.php
+++ wp-content/themes/plaza/header-kg.php
@@ -7,10 +7,10 @@
     <meta name="description" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ;?>/img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() ;?>/img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri() ;?>/img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri() ;?>/img/favicon/apple-touch-icon-114x114.png">
     <!-- Chrome, Firefox OS and Opera -->
     <meta name="theme-color" content="#000">
     <!-- Windows Phone -->
